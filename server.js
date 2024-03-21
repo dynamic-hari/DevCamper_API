d08817e6f408640eb74d3e6e158fd454e407a12c
Hari Haran K S <kshariharan20b@gmail.com>
2024-03-21 12:52:44 +0530

    Updated Connection to MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
 
 // Config port
 const PORT = process.env.PORT || 5000;
 
 // Route Files
 const bootCampsRoutes = require("./routes/bootCamps");
-
-// Load env vars
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
@@ -19,4 +24,4 @@ app.use(morgan("dev"));
 // Mount routers
 app.use("/api/v1/bootCamps", bootCampsRoutes);
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold));
